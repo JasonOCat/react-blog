239ce1fea76d3deda61fa11950db4f1d4c1544c9
JasonOCat <jason.catbagan@gmail.com>
2024-03-07 07:34:11 +0100

    Move call api to the homepage component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
+import prisma from "@/lib/prisma";
+import React from "react";
+import Post from "@/components/Post";
+
 export default async function Home() {
-  const response = await fetch('http://localhost:3000/api/posts', {
-    cache: "no-store"
+  const posts = await prisma.post.findMany({
+    where: {
+      published: true
+    },
+    include: {
+      author: true,
+    },
   })
-  const data = await response.json()
-  console.log(data)
-  return <div>Home</div>
+
+  return (
+      <main>
+        <h1>Last Post</h1>
+        {posts.map(post => (
+            <Post key={post.id} post={post}/>
+        ))}
+      </main>
+  )
 }
