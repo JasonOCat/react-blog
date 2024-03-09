006725f98338abc8be5e4bb618ab922a9eee8380
JasonOCat <jason.catbagan@gmail.com>
2024-03-09 06:40:36 +0100

    Add draft page

--- app/drafts/page.tsx
+++ app/drafts/page.tsx
@@ -1,5 +1,29 @@
-export default function DraftsPage() {
+import prisma from "@/lib/prisma";
+import Post from "@/components/Post";
+import React from "react";
+import styles from './Drafts.module.css';
+
+export default async function DraftsPage() {
+
+    const drafts = await prisma.post.findMany({
+        where: {
+            published: false
+        },
+        include: {
+            author: true,
+        },
+    })
     return(
-        <div>Drafts</div>
+        <main>
+            <h1>Drafts</h1>
+            {drafts.map(post => (
+                <div key={post.id} className={styles.draft}>
+                    <Post post={post}/>
+                </div>
+
+            ))}
+        </main>
     )
+
+
 };
